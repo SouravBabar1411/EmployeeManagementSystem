587832e69b8f7aa1adbb4e0eb17c4fa06062d966
RahulOtari1993 <rahulotari2393@gmail.com>
2022-05-17 22:34:58 +0530

    UI Forgot Password form DONE

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
   
   #validations
-  validates :first_name, :last_name, :date_of_birth, presence: true
+  # validates :first_name, :last_name, :date_of_birth, presence: true
 
   #omniauth google social login
   def self.from_omniauth(auth)
