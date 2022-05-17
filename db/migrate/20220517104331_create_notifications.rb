cd2058d5e1e4e4879dc4e5ab3c0c614a8ed3e143
Deshmukh-Snehal <deshmukhs344@gmail.com>
2022-05-17 16:40:09 +0530

    added migrations and associations

--- db/migrate/20220517104331_create_notifications.rb
+++ db/migrate/20220517104331_create_notifications.rb
@@ -1,9 +1,9 @@
 class CreateNotifications < ActiveRecord::Migration[5.2]
   def change
     create_table :notifications do |t|
-      t.string :title, null: true
-      t.bigint :notificable_id, null: true
-      t.string :notificable_type, null: true
+      t.string :title, null: false
+      t.bigint :notificable_id, null: false
+      t.string :notificable_type, null: false
 
       t.timestamps
     end
