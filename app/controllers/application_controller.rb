340f95b23b3b123d42915d5c663eeaf7ee129df0
Sourav <babarsouav12@gmail.com>
2022-05-17 14:29:41 +0530

    permited devise extra fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  ## To permit additional parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:first_name,:last_name,:date_of_birth,:is_active,:role,:gender,:image,:company_id ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email,:first_name,:last_name,:date_of_birth,:is_active,:role,:gender,:image,:company_id])
+  end
 end
