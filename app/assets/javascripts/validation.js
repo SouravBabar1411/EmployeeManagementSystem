243cb572bfcbd9b53dfa163da838d5b496c710f7
Sourav <babarsouav12@gmail.com>
2022-05-18 17:42:11 +0530

    added frontend validation for password

--- app/assets/javascripts/validation.js
+++ app/assets/javascripts/validation.js
@@ -6,23 +6,35 @@ $(document).on('turbolinks:load', function() {
             },
             'user[password]': {
                 required: true,
+                pwcheck: true,
+                minlength: 6
             },
             'user[password_confirmation]': {
                 required: true,
+                equalTo: "#pass"
             }
         },
         messages: {
-            'user[email]': {
-                required: "Please enter email address",
-            },
             'user[password]': {
-                required: "Please enter password"
+                required: "Please enter your password ",
+                pwcheck: "Must include at least one lowercase letter, one uppercase letter, one digit and special character"
             },
             'user[password_confirmation]': {
-                required: "Please enter confirm password"
+                required: "Please enter Confirm Password",
+                equalTo: "Confirm Password Not Matched"
             }
         }
     });
+
+    $.validator.addMethod("pwcheck",
+        function(value, element) {
+            return /^[a-zA-Z0-9!@#$%^&*()_=\[\]{};':"\\|,.<>\/?+-]+$/.test(value) &&
+                /[a-z]/.test(value) // has a lowercase letter
+                &&
+                /\d/.test(value) //has a digit
+                &&
+                /[!@#$%^&*()_=\[\]{};':"\\|,.<>\/?+-]/.test(value) //has one special character
+        });
 });
 $(document).on('turbolinks:load', function() {
     $("#login").validate({
