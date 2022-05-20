f089f96ceed85abb9644a99464711db19ab893c3
@pratiksha.itworks <pratiksha.itworks@gmail.com>
2022-05-20 22:07:48 +0530

    added helper method for jobs actions

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,11 @@
 module JobsHelper
+  def action_text
+    if controller.action_name == "new"
+       return "Add Job"
+    elsif controller.action_name == "edit"
+       return "Update Job"
+    else
+       return "Submit"
+    end
+  end
 end
