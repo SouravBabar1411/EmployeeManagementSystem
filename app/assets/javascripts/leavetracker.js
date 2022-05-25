1059c2f953559cc8fec5573e5a4b0746fdaa9f75
Sourav <babarsouav12@gmail.com>
2022-05-25 23:34:00 +0530

    updated code with action buttons for leave tracker

--- app/assets/javascripts/leavetracker.js
+++ app/assets/javascripts/leavetracker.js
@@ -30,6 +30,41 @@ $(document).on('turbolinks:load', function() {
               title: "status",
               data: "is_approved" 
             },
+            {
+              title: 'Is Approve',
+              data: null,
+              render: function(data, type, row) {
+                if (data.is_approved == 1)
+                  return '<i class="bx bxs-check-circle" style="color:#43a430"></i>'
+                else
+                  return '<i class="bx bxs-check-circle" style="color:#c5c9c4"></i>'
+              }
+            },
+            {
+              title: 'Actions',
+              data: null,
+              searchable: false,
+              orderable: false,
+              render: function(data, type, row) {
+                let action_html = "<div class='input-group' data-leavetracker-id ='" + data.id + "'>" +
+                    "<button type='button' class='btn p-0 ' data-bs-toggle='dropdown'>" +
+                    "<i class='bx bx-dots-vertical-rounded'></i></button>" +
+                    "<div class='dropdown-menu'>"
+
+                    // Edit laeavetracker Button  
+                action_html = action_html + "<a class='dropdown-item btn-sm' href = '/leave_trackers/" + data.id + "/edit'" +
+                    " data-toggle='tooltip' data-placement='top' data-original-title='Edit'>" +
+                    "<i class='bx bx-edit-alt me-1'></i> Edit</a>"
+
+                // delete leavetracker
+                action_html = action_html + "<a class='dropdown-item delete-user' href = '/leave_trackers/" + data.id +
+                    "data-confirm='Are you sure?' data-method='delete' >" +
+                    "<i class='bx bx-trash me-1'></i>Delete</a>"
+                action_html = action_html + "</div></div>"
+
+                return action_html;
+              }
+            }
           ],
     aLengthMenu: [[5, 10, 15, 20], [5, 10, 15, 20]],      
     order: [
@@ -52,18 +87,15 @@ $(document).on('turbolinks:load', function() {
           }
       },
       messages: {
-          'timesheet[time]': {
-              required: 'Select Time'
+          'leave_tracker[from_date]': {
+              required: 'Select Date'
           },
-          'timesheet[description]': {
-              required: 'Enter description'
+          'leave_tracker[to_date]': {
+              required: 'Select to date'
           },
-          'timesheet[project_id]': {
-              required: 'Select Project'
-          },
-          'timesheet[job_id]': {
-              required: 'Select Job'
-          },
+          'leave_tracker[reason]': {
+              required: 'Please enter reason'
+          }
       },
       errorPlacement: function(error, element) {
           var placement = $(element).data('error');
