c0dae22ece3106f4b9ab5c2b4721e6cdf24aee09
Sourav <babarsouav12@gmail.com>
2022-05-25 11:28:00 +0530

    address helper modified

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,11 +1,3 @@
 module AddressesHelper
-  def button_text
-    if controller.action_name == "new"
-       return "Add Address"
-    elsif controller.action_name == "edit"
-       return "Update Address"
-    else
-       return "Submit"
-    end
-  end
+  
 end
