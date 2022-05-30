5be6f49c2d8847a604c740fbd993a6f330da054c
Deshmukh-Snehal <deshmukhs344@gmail.com>
2022-05-30 16:05:39 +0530

    remove unnecessary code from timesheet.js

--- app/assets/javascripts/timesheet.js
+++ app/assets/javascripts/timesheet.js
@@ -163,10 +163,6 @@ $(document).on('turbolinks:load', function() {
             $('#timesheet-list-table').DataTable().ajax.reload();
         }
     }
-<<<<<<< HEAD
-=======
-  }
->>>>>>> active-admin
 
     // timesheet filter
     $('.timesheet-filter').change(function() {
