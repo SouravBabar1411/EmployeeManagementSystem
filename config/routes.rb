9cc1aec16306fb9dcaf51e2290de69e3b0923d7a
Sourav <babarsouav12@gmail.com>
2022-06-01 17:49:20 +0530

    added annotation for route

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  # Api routes
+  # Api routes for leave_tracker
   namespace :api do
     namespace :v1 do
       resources :leave_trackers
     end
   end
-  # Api Routes
+
+  # Api Routes for timesheet
   namespace :api do
     namespace :v1 do
       resources :timesheets
